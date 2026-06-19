78cecd26b827b5ef9146e75fbcf441339d35d233
kawt0608 <kaw683129@gmail.com>
2026-06-19 10:47:17 +0900

    Update senser.py

--- senser.py
+++ senser.py
@@ -1,11 +1,4 @@
-{
-  "created_at": "2026-06-19 10:30:00",
-  "sensor_setting": {
-    "trig_pin": 14,
-    "echo_pin": 15,
-    "detect_distance_cm": 50.0,
-    "release_distance_cm": 70.0,
-    "measure_interval_sec": 0.2import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import sys
 import json
@@ -256,45 +249,3 @@ except KeyboardInterrupt:
 
     GPIO.cleanup()
     sys.exit()
-  },
-  "calibration_results": [],
-  "all_measurements": [
-    {
-      "time": "2026-06-19 10:30:01.100",
-      "distance_cm": 120.4,
-      "status": "success",
-      "object_id": null
-    },
-    {
-      "time": "2026-06-19 10:30:01.300",
-      "distance_cm": 45.2,
-      "status": "success",
-      "object_id": 1
-    }
-  ],
-  "objects": [
-    {
-      "object_id": 1,
-      "start_time": "2026-06-19 10:30:01.300",
-      "end_time": "2026-06-19 10:30:02.100",
-      "samples": [
-        {
-          "time": "2026-06-19 10:30:01.300",
-          "distance_cm": 45.2
-        },
-        {
-          "time": "2026-06-19 10:30:01.500",
-          "distance_cm": 42.8
-        },
-        {
-          "time": "2026-06-19 10:30:01.700",
-          "distance_cm": 39.6
-        }
-      ],
-      "sample_count": 3,
-      "min_distance_cm": 39.6,
-      "max_distance_cm": 45.2,
-      "average_distance_cm": 42.5
-    }
-  ]
-}
